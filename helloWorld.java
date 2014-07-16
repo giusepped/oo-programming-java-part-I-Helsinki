7f8bdb95138b8363b501a36d9a2aa9dc5671b488
giusepped <giusdesan@gmail.com>
2014-07-16 19:04:53 +0100

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("(And all the people of the world)"):
+    System.out.println("(And all the people of the world)");
     }
   }
