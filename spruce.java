9098bfccdffdd28159f422e187ecc4c40719f318
giusepped <giusdesan@gmail.com>
2014-07-16 11:06:52 +0100

    Update spruce.java

--- spruce.java
+++ spruce.java
@@ -1,5 +1,10 @@
 public class Spruce {
   pubilc static void main (String[] args){
     System.out.println("    *");
+    System.out.println("   ***");
+    System.out.println("  *****");
+    System.out.println(" *******");
+    System.out.println("*********");
+    System.out.println("    *    ");
     }
   }
