e88a0e380dc1e7ffc1a3e3aa1b517513dfe6b21f
giusepped <giusdesan@gmail.com>
2014-07-16 11:50:42 +0100

    Update varyingVariables.java

--- varyingVariables.java
+++ varyingVariables.java
@@ -1,6 +1,14 @@
 public class varyingVariables {
   public static void main(String args []){
     int chickens = 3;
-    
+    double bacon = 5.5;
+    String tractor = "There is none!";
+    System.out.println("Chickens: \n" + chickens + "\nBacon (kg): \n" + bacon + "\nA tractor:\n" + tractor);
+    System.out.println("In a nutshell: \n" + chickens + bacon + tractor);
+    chickens = 9000;
+    bacon = 0.1;
+    tractor = "Zetor";
+    System.out.println("Chickens: \n" + chickens + "\nBacon (kg): \n" + bacon + "\nA tractor:\n" + tractor);
+    System.out.println("In a nutshell: \n" + chickens + bacon + tractor);
     }
   }
