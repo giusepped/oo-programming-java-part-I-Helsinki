a1b5a8756bc11cd864e181eb6fef11c19b9fa7ad
giusepped <giusdesan@gmail.com>
2014-07-17 19:42:48 +0100

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+        
+        int a = 1337;
+        int b = 42;
+
+        int result = a * b;
+        System.out.println(a +" * "+ b +" = "+ result);
+    }
+
+}
