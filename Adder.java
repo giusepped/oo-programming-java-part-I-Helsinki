45aeb42fc3a02edf8d4bd10e13bd868674c2add9
giusepped <giusdesan@gmail.com>
2014-07-18 21:30:30 +0100

    Create Adder.java

---
+++ Adder.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Adder {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        // Implement your program here. Remember to ask the input from user
+        System.out.println("Type a number: ");
+        int number1 = Integer.parseInt(reader.nextLine());
+        System.out.println("Type another number: ");
+        int number2 = Integer.parseInt(reader.nextLine());
+        int number3 = number1 + number2;
+        System.out.println("Sum of the number: " + number3);
+    }
+}
