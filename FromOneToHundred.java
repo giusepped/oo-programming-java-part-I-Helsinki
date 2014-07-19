eda66def1eb569c36cab8691c648f9cc391f5a49
giusepped <giusdesan@gmail.com>
2014-07-19 19:39:37 +0200

    Create FromOneToHundred.java

---
+++ FromOneToHundred.java
@@ -0,0 +1,12 @@
+
+public class FromOneToHundred {
+
+    public static void main(String[] args) {
+        // Write your program here
+        int number = 1;
+        while (number <101){
+            System.out.println(number);
+            number++;
+        }
+    }
+}
