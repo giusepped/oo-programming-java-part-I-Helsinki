a8eadb875f3c0d4ebb190598dfee14ca95b418f4
giusepped <giusdesan@gmail.com>
2014-07-21 11:48:46 +0200

    Create Greatest.java

---
+++ Greatest.java
@@ -0,0 +1,20 @@
+public class Greatest {
+
+    public static int greatest(int number1, int number2, int number3) {
+
+        if(number1 > number2 && number1 > number3){
+            return number1;
+        }else if(number2 > number1 && number2 > number3){
+            return number2;
+        }else if(number3 > number1 && number3 > number2){
+            return number3;
+        }else{
+            return number1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int result = greatest(2, 7, 3);
+        System.out.println("Greatest: " + result);
+    }
+}
