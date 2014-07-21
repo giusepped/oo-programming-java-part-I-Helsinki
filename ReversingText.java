00718163346ecd7c9a639ce828750516b9d34af7
giusepped <giusdesan@gmail.com>
2014-07-21 18:26:48 +0200

    Create ReversingText.java

---
+++ ReversingText.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class ReversingText {
+
+    public static String reverse(String text) {
+        // write your code here
+        // note that method does now print anything, it RETURNS the reversed string
+        String help = "";
+        int i = text.length() - 1;
+        while(i >= 0){
+            help = help + text.charAt(i);
+            i--;
+        }
+        return help;
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type in your text: ");
+        String text = reader.nextLine();
+        System.out.println("In reverse order: " + reverse(text));
+    }
+}
