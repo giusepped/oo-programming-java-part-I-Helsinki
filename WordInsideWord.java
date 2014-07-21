c119b341430100a9409ece2f5c6844cd4ef8e71a
giusepped <giusdesan@gmail.com>
2014-07-21 18:25:46 +0200

    Create WordInsideWord.java

---
+++ WordInsideWord.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class WordInsideWord {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type the first word: ");
+        String firstWord = reader.nextLine();
+        System.out.println("Type the second word: ");
+        String secondWord = reader.nextLine();
+        if (firstWord.indexOf(secondWord) != -1){
+            System.out.println("The word '"+ secondWord +" is found in the word '"+ firstWord +"'.");
+        }else{
+            System.out.println("The word '"+ secondWord +" is not found in the word '"+ firstWord +"'.");
+        }
+    }
+}
