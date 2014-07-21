b64cd23b54efdacdbfa31accef731dfe00166031
giusepped <giusdesan@gmail.com>
2014-07-21 18:19:15 +0200

    Create LengthOfName.java

---
+++ LengthOfName.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class LengthOfName {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        // call your method from here
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
+        System.out.println("Number of characters: " + calculateCharacters(name));
+    }
+    
+    // do here the method
+    // public static int calculateCharacters(String text)
+    public static int calculateCharacters(String text){
+        return text.length();
+    }
+}
