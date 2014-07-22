c232e2a22c08b192d16a59974a56ede5f5490b97
giusepped <giusdesan@gmail.com>
2014-07-22 11:03:59 +0200

    Create RecurringWord.java

---
+++ RecurringWord.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RecurringWord {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        // create here the ArrayList 
+        ArrayList<String> words = new ArrayList<String>();
+        while(true){
+            System.out.println("Type a word: ");
+            String word = reader.nextLine();
+            if(words.contains(word)){
+                System.out.println("You gave the word "+word+" twice.");
+                break;
+            }
+            words.add(word);  
+        }
+        
+    }
+}
