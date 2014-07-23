87ebb41e5be51112140ea098200b6736e681b34f
giusepped <giusdesan@gmail.com>
2014-07-23 15:28:19 +0200

    Create Variance.java

---
+++ Variance.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Variance {
+
+    public static int sum(ArrayList<Integer> list) {
+        int sum = 0;
+        for (int l : list){
+            sum += l;
+        }
+        return sum;
+    }
+    
+
+    public static double average(ArrayList<Integer> list) {
+        double average = sum(list) / (double)(list.size());
+        return average;
+    }
+
+    public static double variance(ArrayList<Integer> list) {
+
+        double num = 0;
+        for ( int l : list){
+            num += (l - average(list))*(l - average(list));
+        }
+        double denom = (double)(list.size()) - 1.0;
+        double var = num/denom;
+        return var;
+    }
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(2);
+        list.add(7);
+        list.add(2);
+        
+        System.out.println("The variance is: " + variance(list));
+    }
+
+}
