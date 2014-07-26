ada5b22d4e47f98a8fb473eaf1a6eb239ae3099e
giusepped <giusdesan@gmail.com>
2014-07-26 13:11:27 +0200

    Create DiceClass.java

---
+++ DiceClass.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Dice {
+    private Random random = new Random();
+    private int numberOfSides;
+
+    public Dice(int numberOfSides) {
+        // Initialize here the number of sides
+        this.numberOfSides = numberOfSides;
+    }
+
+    public int roll() {
+        // create here a random number belongig to range 1-numberOfSided
+        int randomNumber = random.nextInt(this.numberOfSides);
+        return randomNumber + 1;
+    }
+}
