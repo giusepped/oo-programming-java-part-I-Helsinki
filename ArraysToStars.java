5cfe1f7389b8a88e8e28f0e070315e5a93764c8b
giusepped <giusdesan@gmail.com>
2014-07-29 14:08:05 +0200

    Create ArraysToStars.java

---
+++ ArraysToStars.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // test the method here
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayAsStars(array);
+    }
+
+    public static void printArrayAsStars(int[] array) {
+        // write code here
+        for(int i : array){
+            for(int j = 0; j < i; j ++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
